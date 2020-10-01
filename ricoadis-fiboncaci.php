80b72e4692dbc7ab5400c1e1eb67e7ec2f2b7105
ricoadis <72136140+ricoadis@users.noreply.github.com>
2020-10-01 14:03:00 +0700

    Menambahkan penjelasan di setiap nilai variable

--- ricoadis-fiboncaci.php
+++ ricoadis-fiboncaci.php
@@ -1,13 +1,20 @@
 <?php
+	// siapkan 2 angka awal
+	//$x=0;
+	//$y=1;
 	$x = 0;
 	$y = 1;
+
+	//tampilkan 2 angka awal
 	echo "$x $y";
 	 
 	for ($i=0; $i<8; $i++)
 	{
+		// hitung angka yang akan ditampilkan
 		$output = $y + $x;
 		echo " $output";
-
+		
+		//siapkan angka untuk perhitungan berikutnya
 		$x = $y;
 		$y = $output;
 	}
